5a886726972770dddf6cfd0274d0f0502c181501
Risto Pejasinovic <risto.pejasinovic@gmail.com>
2019-07-09 19:06:17 +0200

    Verilator timeout issue test

---
+++ timeout_test/run_sim.py
@@ -0,0 +1,40 @@
+from pygears import gear, Intf
+from pygears.typing import Uint
+from pygears.conf.registry import registry, bind
+
+from pygears.lib.verif import drv
+from pygears.lib.verif import directed
+from pygears.sim.modules.verilator import SimVerilated
+from pygears.sim import sim
+from functools import partial
+
+from pygears.lib import dreg
+from pygears.lib import data_dly
+from pygears.lib import shred
+
+
+@gear
+def data_dly(din, *, len):
+    dout = din
+    for i in range(len):
+        dout = dout | dreg
+
+    return dout
+
+
+@gear
+def timeout_test(din):
+    dout = din | data_dly(len=110)
+
+    return dout
+
+
+bind('hdl/debug_intfs', ['*'])
+
+seq = list(range(10))
+
+directed(
+    drv(t=Uint[8], seq=seq),
+    f=timeout_test(sim_cls=SimVerilated),
+    ref=seq)
+sim(outdir="build")
